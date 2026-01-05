840694275959107924f61e3b829b07fa402156b4
Arut0ria <anoxing64@gmail.com>
2026-01-05 18:44:47 +0100

    build(Project): Adding mdx, use-gestures and svgr to project, including plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mdx from '@mdx-js/rollup'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    mdx(),
+    svgr({
+      include: "**/icons/*.svg",
+      svgrOptions: {
+        icon: true,
+        exportType: "default"
+      }
+    })
+  ],
 })

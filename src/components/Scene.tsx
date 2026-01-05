6a0ee6fffc83b2f6de866301a4636d553395edf2
Arut0ria <anoxing64@gmail.com>
2026-01-05 18:56:21 +0100

    feat(Scene): Updating main scene component to include markdown pages and blocklist

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,14 +1,37 @@
 import { Canvas } from "@react-three/fiber";
-import { Color } from "three";
-import Background from "./Background";
-import { OrbitControls } from "@react-three/drei";
+import Background from "./Background/Background";
+
+import { useState } from "react";
+import BlockList from "./BlockList/BlockList";
+import MarkdownPage from "./MardownPage/MarkdownPage";
+import data_json from "/src/assets/data.json?raw";
 
 export default function Scene() {
-  return (
-    <Canvas>
+  const data = JSON.parse(data_json);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  return (<>
+    <Canvas
+      gl={{
+        antialias: false,
+        powerPreference: "high-performance",
+      }}
+      dpr={[1, 1.5]}
+    >
       <Background />
-      <ambientLight intensity={1.0} color={new Color(1, 1, 1)} />
-      <OrbitControls />
+      <BlockList
+        items={data["data"]}
+        currentIndex={currentIndex}
+        onIndexChange={setCurrentIndex}
+      />
+      {/* <ambientLight intensity={2.0} color={new Color(1, 1, 1)} /> */}
+      {/* <OrbitControls /> */}
     </Canvas>
-  );
+    <MarkdownPage
+      path={
+        data["data"][currentIndex]["content"]
+      }
+    />
+  </>);
 }
